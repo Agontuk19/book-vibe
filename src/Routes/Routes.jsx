ee9217bc50b48e594bde28c1cb45dcaaa562c24b
Agontuk19 <khandokertanjim@gmail.com>
2026-02-10 22:08:51 +0600

    Web Home Deploy

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { createBrowserRouter } from "react-router";
+import { createHashRouter } from "react-router";
 import Root from '../pages/Root/Root';
 import ErrorPage from '../pages/ErrorPage/ErrorPage';
 import Home from '../pages/Home/Home';
 import BookDetails from '../pages/BookDetails/BookDetails';
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     Component: Root,
@@ -18,7 +18,7 @@ const router = createBrowserRouter([
         },
         {
             path: '/bookDetails/:id',
-            loader: () => fetch('./booksData.json'),
+            loader: () => fetch('/booksData.json'),
             Component: BookDetails
         }
     ]
